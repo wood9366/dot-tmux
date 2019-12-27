58fefb9a95e1bd870ea1117e5bca8cc778b702a3
Li Yang <liyang@Lis-MacBook-Pro.local>
2019-12-27 08:40:14 +0800

    克隆tmux的配置之前检查目录是否存在，添加tmux插件管理tpm的克隆

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/wood9366/dot-tmux.git ~/.dot-tmux
-ln -s ~/.dot-tmux/.tmux.conf ~/
+DOT_TMUX="$HOME/.dot-tmux"
+
+if [ ! -e "$DOT_TMUX" ]; then
+    git clone https://github.com/wood9366/dot-tmux.git "$DOT_TMUX"
+fi
+
+TPM="$HOME/.tmux/plugins/tpm"
+
+if [ ! -e "$TPM" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$TPM"
+fi
+
+ln -s "$DOT_TMUX/.tmux.conf" "$HOME"
